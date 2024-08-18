b03b412435cfef4f712a13baa69d4890dba0e17b
Claudius Joel <otiiclaudius@gmail.com>
2024-08-18 16:54:16 +0300

    connect backend

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,4 +1,9 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = "Hi , this is a simple chatbot. You said: " + message + ". I'm here to join you on the   server and help you. "
+        return JsonResponse({'response': response})
     return render(request, 'chatbot.html')
